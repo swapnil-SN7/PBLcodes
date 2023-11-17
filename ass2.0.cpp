36017a93291d2057ac38fc80feccaec478494f53
Shreyash7484 <shindeshreyah2900@gmail.com>
2023-11-17 12:07:14 +0530

    sort code

--- ass2.0.cpp
+++ ass2.0.cpp
@@ -1,2 +1,33 @@
-#include<iostream>
-using 
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+
+void sortAlphabetically(string names[], int n) {
+    sort(names, names + n);
+    for (int i = 0; i < n; i++) {
+        cout << names[i] << endl;
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter the number of names you want to add:" << endl;
+    cin >> n;
+    cin.ignore(); // Clear the newline character from the input buffer
+
+    string names[n];
+    for (int i = 0; i < n; i++) {
+        cout << "Name: ";
+        getline(cin, names[i]);
+    }
+
+    cout << "Before sorting:" << endl;
+    for (int j = 0; j < n; j++) {
+        cout << names[j] << endl;
+    }
+
+    cout << "After sorting:" << endl;
+    sortAlphabetically(names, n);
+
+    return 0;
+}
